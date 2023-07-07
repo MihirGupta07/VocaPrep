19869a3f532df9dc88f962eaafab1dedb09cbde4
MihirGuptaByjus <mihir.gupta@byjus.com>
2023-07-07 23:02:57 +0530

    VocaPrep

---
+++ firebaseConfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyBxAUUQs4HMdkTEJ_BGIOvNoEUE9dUPTdY",
+    authDomain: "vocaprep.firebaseapp.com",
+    projectId: "vocaprep",
+    storageBucket: "vocaprep.appspot.com",
+    messagingSenderId: "900861427214",
+    appId: "1:900861427214:web:a0b3edb336aaa7cf95c65f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
